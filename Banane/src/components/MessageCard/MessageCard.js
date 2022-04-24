b7c4c3ec23e3588cc3bd76b070ee2207ec1ee621
Muhammed Mertoğlu <av.muhammedmertoglu@gmail.com>
2022-04-24 15:56:32 +0300

    add tr locale to date-fns

--- Banane/src/components/MessageCard/MessageCard.js
+++ Banane/src/components/MessageCard/MessageCard.js
@@ -2,9 +2,13 @@ import React from "react";
 import { View, Text } from 'react-native';
 import styles from './MessageCard.style'
 import { format, formatDistance, formatRelative, parseISO, subDays } from 'date-fns'
+import {tr} from 'date-fns/locale'
 
 const MessageCard = ({ message }) => {
-    const formatteddate = formatDistance(parseISO(message.date), new Date(), { addSuffix: true })
+    const formatteddate = formatDistance(parseISO(message.date), new Date(), { 
+        addSuffix: true,
+        locale:tr
+    })
     return (
         <View style={styles.container} >
             <View style={styles.inner_container} >
