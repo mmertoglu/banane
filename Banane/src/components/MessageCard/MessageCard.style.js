1fcb5c1bd05dc9da4dcb903bb0680be2670680d7
Muhammed Mertoğlu <av.muhammedmertoglu@gmail.com>
2022-04-25 14:26:17 +0300

    style changed

--- Banane/src/components/MessageCard/MessageCard.style.js
+++ Banane/src/components/MessageCard/MessageCard.style.js
@@ -29,6 +29,37 @@ export default StyleSheet.create({
         borderBottomWidth:1,
         paddingBottom:2,
         borderBottomColor:colors.lightpink    
+    },
+    bananebutton:{
+        alignSelf:'flex-end',
+        backgroundColor:colors.lightpink,
+        paddingVertical:2,
+        paddingHorizontal:15,
+        borderRadius:8,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center'
+    },
+    bananetext:{
+        color:'black',
+        marginLeft:4,
+        
+        marginRight:6
+    },
+    disliketext:{
+        color:colors.lightpink,
+        fontSize:12
+    },
+    icon:{
+        marginTop:2
+    },
+    dislikecontainer:{
+        backgroundColor:colors.darkpink,
+        width:40,
+        alignItems:'center',
+        borderRadius:80,
+        padding:2,
+        
     }
 
 })
